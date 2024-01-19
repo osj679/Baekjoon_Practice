92215a434a5caa6d5191f484c42b370176f5bb76
osj679 <ohsj3781@gmail.com>
2024-01-19 14:22:16 +0900

    finish 21736

--- 21736/21736.cpp
+++ 21736/21736.cpp
@@ -16,8 +16,9 @@ void input_matrix(std::vector<std::vector<char>> &matrix)
 
 const std::string find_ans(const std::vector<std::vector<char>> &matrix)
 {
-    static const std::pair<int, int> init_pair(-1, -1);
-    static const std::vector<const std::pair<const int, const int>> move = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int ans = 0;
+    std::pair<int, int> init_pair(-1, -1);
+    const std::vector< std::pair<int, int>> move = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     std::pair<int, int> pos = {-1, -1};
     for (int i = 0; i < matrix.size() && pos == init_pair; ++i)
@@ -26,14 +27,48 @@ const std::string find_ans(const std::vector<std::vector<char>> &matrix)
         {
             if (matrix[i][j] == 'I')
             {
-                pos.first = j;
-                pos.second = i;
+                pos.first = i;
+                pos.second = j;
             }
         }
     }
 
-    
+    std::vector<std::vector<int>> route_save(matrix.size(), std::vector<int>(matrix[0].size(), -1));
 
+    std::queue<std::pair<int, int>> q;
+    q.push(pos);
+    route_save[pos.first][pos.second] = 0;
+
+    while (!q.empty())
+    {
+        const std::pair<int, int> now_pos(q.front());
+        q.pop();
+        if (matrix[now_pos.first][now_pos.second] == 'P')
+        {
+            ++ans;
+        }
+        for (int i = 0; i < 4; ++i)
+        {
+            std::pair<int, int> next_pos(now_pos);
+            next_pos.first += move[i].first;
+            next_pos.second += move[i].second;
+            if (next_pos.first < 0 || next_pos.first >= matrix.size() || next_pos.second < 0 || next_pos.second >= matrix[0].size() || matrix[next_pos.first][next_pos.second] == 'X' || route_save[next_pos.first][next_pos.second] != -1)
+            {
+                continue;
+            }
+            route_save[next_pos.first][next_pos.second] = route_save[now_pos.first][now_pos.second] + 1;
+            q.push(next_pos);
+        }
+    }
+
+    if (ans == 0)
+    {
+        return "TT";
+    }
+    else
+    {
+        return std::to_string(ans);
+    }
 }
 
 int main()

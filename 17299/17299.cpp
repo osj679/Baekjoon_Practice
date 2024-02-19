ecd05cff3eee852b4ab37f281baaac282033aaf5
ohsj3781 <ohsj3781@gmail.com>
2024-02-19 11:56:36 +0900

    finish 17299

--- 17299/17299.cpp
+++ 17299/17299.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
-static const int MAX=1'000'000;
+static const int MAX = 1'000'000;
 
 int main()
 {
@@ -12,13 +13,32 @@ int main()
     std::cin >> N;
 
     std::vector<int> v(N);
-    std::vector<int> cnt(MAX+1,0);
-    for(int i=0;i<v.size();++i){
+    std::vector<int> cnt(MAX + 1, 0);
+    for (int i = 0; i < v.size(); ++i)
+    {
         int num;
-        std::cin>>num;
-        v[i]=num;
+        std::cin >> num;
+        v[i] = num;
         cnt[num]++;
     }
 
+    std::vector<int> ans(N, -1);
+    std::stack<int> s;
 
+    for (int i = v.size() - 1; i >= 0; --i)
+    {
+        while (!s.empty() && cnt[v[i]] >= cnt[s.top()])
+            s.pop();
+
+        if (!s.empty())
+            ans[i] = s.top();
+        s.push(v[i]);
+    }
+
+    for (auto i = ans.begin(); i != ans.end(); ++i)
+    {
+        std::cout << *i << ' ';
+    }
+
+    return 0;
 }
